b6772b0e86a34dd4e897f3706140669381bae5a1
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2023-07-03 19:17:18 +0300

    semi

---
+++ frontend/src/components/Question.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import '../stylesheets/Question.css';
+
+class Question extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      visibleAnswer: false,
+    };
+
+
+  }
+
+  flipVisibility() {
+    this.setState({ visibleAnswer: !this.state.visibleAnswer });
+  }
+  render() {
+  
+  const { question, answer, category, difficulty } = this.props;
+
+    return (
+      <div className='Question-holder'>
+        <div className='Question'>{question}</div>
+        <div className='Question-status'>
+          <img
+            className='category'
+            alt={`${category.toLocaleLowerCase()}`}
+            src={`${category.toLocaleLowerCase()}.svg`}
+            // alt={`${Object.values(category)[0].toLocaleLowerCase()}`}
+            // src={`${Object.values(category)[0].toLocaleLowerCase()}.svg`}
+          />
+          <div className='difficulty'>Difficulty: {difficulty}</div>
+          <img
+            src='delete.png'
+            alt='delete'
+            className='delete'
+            onClick={() => this.props.questionAction('DELETE')}
+          />
+        </div>
+        <div
+          className='show-answer button'
+          onClick={() => this.flipVisibility()}
+        >
+          {this.state.visibleAnswer ? 'Hide' : 'Show'} Answer
+        </div>
+        <div className='answer-holder'>
+          <span
+            style={{
+              visibility: this.state.visibleAnswer ? 'visible' : 'hidden',
+            }}
+          >
+            Answer: {answer}
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Question;
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      query: '',
+    };
+  
+  }
+
+  getInfo = (event) => {
+    event.preventDefault();
+    this.props.submitSearch(this.state.query);
+  };
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value,
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.getInfo}>
+        <input
+          placeholder='Search questions...'
+          ref={(input) => (this.search = input)}
+          onChange={this.handleInputChange}
+        />
+        <input type='submit' value='Submit' className='button' />
+      </form>
+    );
+  }
+}
+
+export default Search;
